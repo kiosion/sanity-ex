7cdc7192407df6df45c3fd505442df1093b686c0
kiosion <hi@kio.dev>
2023-06-16 22:43:08 -0300

    chore: Update mix.exs fields

--- mix.exs
+++ mix.exs
@@ -4,21 +4,38 @@ defmodule Sanityex.MixProject do
   def project do
     [
       app: :sanityex,
+      name: "SanityEx",
       version: "0.1.0",
       elixir: "~> 1.14",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: description(),
+      source_url: "https://github.com/kiosion/sanity-ex"
     ]
   end
 
   def application do
+    []
+  end
+
+  defp package do
     [
-      extra_applications: [:logger]
+      files:
+        ~w(lib mix.exs README* readme* LICENSE* license* CHANGELOG* changelog* src .formatter.exs),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/kiosion/sanity-ex"}
     ]
+  end
+
+  defp description do
+    "A client for interacting with the Sanity API and constructing GROQ queries from Elixir applications."
   end
 
   defp deps do
     [
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false},
       {:httpoison, "~> 1.8"},
       {:jason, "~> 1.2"},
       {:mox, "~> 1.0", only: :test}
